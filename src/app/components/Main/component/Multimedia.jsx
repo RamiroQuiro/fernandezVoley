0c73fff9028f2380972fa439c0e5a351effaa2e6
ramirochangomoreno <rama.exe.13@gmail.com>
2023-04-04 15:11:39 -0300

    implementando galeria

--- src/app/components/Main/component/Multimedia.jsx
+++ src/app/components/Main/component/Multimedia.jsx
@@ -3,48 +3,73 @@ import React from "react";
 
 export default function Multimedia() {
   return (
-    <section className="pb-28">
-      <div className="container  flex items-center justify-center gap-2 mx-auto">
-        <div className="w-1/2 max-h-full flex-col bg-red-500">
-          <div className=" grid w-full grid-cols-2 h-1/2 grid-rows-2 gap-2">
-          <Image
-            quality={50}
-            sizes="320"
-            fill
-            alt={"title"}
-            src="https://source.unsplash.com/random/301x301/"
-            className=" object-cover  object-center rounded"
-          />
+    <section className="py-14 ">
+         <div className="container mt-14 flex items-center justify-center gap-2 mx-auto">
+      <h2 className="  md:text-4xl  text-start text-2xl z-10 font-extrabold leading-none tracking-tight  lg:text-5xl text-transparent bg-clip-text bg-gradient-to-r  to-cyan-600 from-blue-700">
+        Galeria{" "}
+      </h2>
+      </div>
+      <div className="container mt-14 flex items-center justify-center  mx-auto">
+        <div className="w-1/2 max-h-full flex-col items-center mx-auto  py-3 gap-3">
+          <div className="  w-full flex flex-wrap gap-2 items-center justify-center mx-auto">
             <img
-              className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
-             src="https://source.unsplash.com/random/301x301/"
-              alt="ss"
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover w-[45%] rounded   bg-gray-500"
+            />{" "}
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover  w-[45%] rounded  bg-gray-500"
+            />{" "}
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover  w-[45%] rounded  bg-gray-500"
+            />{" "}
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover  w-[45%] rounded  bg-gray-500"
             />
-            <img alt="" src="https://source.unsplash.com/random/200x200/?0" />
-            <img src="https://source.unsplash.com/random/301x301/" alt="" />
-            <img alt="" src="https://source.unsplash.com/random/200x200/?0" />
           </div>
-          <div className="h-1/2 w-full">
+          <div className="h-1/2 p-6 w-full mx-auto">
             <img
               alt=""
-              className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
+              className="w-full h-auto object-cover rounded mx-auto"
               src="https://source.unsplash.com/random/200x200/?7"
             />
           </div>
         </div>
-        <div className="w-1/2 flex-col bg-green-500">
-          <div>
+        <div className="w-1/2 max-h-full flex-col items-center mx-auto  py-3 gap-3">
+          <div className="h-1/2 pb-6 px-6 w-full mx-auto">
             <img
               alt=""
-              className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
+              className="w-full h-auto object-cover rounded mx-auto"
               src="https://source.unsplash.com/random/200x200/?7"
             />
           </div>
-          <div className=" grid grid-cols-2 grid-rows-2 gap-2">
-            <img src="https://source.unsplash.com/random/301x301/" alt="" />
-            <img alt="" src="https://source.unsplash.com/random/200x200/?0" />
-            <img src="https://source.unsplash.com/random/301x301/" alt="" />
-            <img alt="" src="https://source.unsplash.com/random/200x200/?0" />
+          <div className=" gap-2 w-full flex flex-wrap  items-center justify-center mx-auto">
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover w-[45%] rounded   bg-gray-500"
+            />{" "}
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover  w-[45%] rounded  bg-gray-500"
+            />{" "}
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover  w-[45%] rounded  bg-gray-500"
+            />{" "}
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              className="object-cover  w-[45%] rounded  bg-gray-500"
+            />
           </div>
         </div>
       </div>
