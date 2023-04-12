bf2304443cce11814161e57193fbc386bce1ad33
ramirochangomoreno <rama.exe.13@gmail.com>
2023-04-12 10:12:10 -0300

    componente imageNext

--- src/app/components/Main/component/FilasGaleria.jsx
+++ src/app/components/Main/component/FilasGaleria.jsx
@@ -12,12 +12,13 @@ export default function FilasGaleria({ arrayImagenes }) {
             arrayImagenes?.slice(0,2).map((img,i)=>(
               <div 
               key={i}
-              className="relative hover:saturate-100 sature-50 rounded  hover:border-orange-500/80 bg-transparent duration-150 hover:border-dotted border-2 border-inherit">
+              className="relative w-1/2 hover:saturate-100 sature-50 rounded  hover:border-orange-500/80 bg-transparent duration-150 hover:border-dotted border-2 border-inherit">
               <Image
-              fill
-              src={img.image_url || ""}
+              width={500}
+              height={500}
+              src={img.image_url}
               alt={"foto"+i}
-              className="object-cover w-full duration-200 cursor-pointer hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg shadow-md rounded hover: dark:bg-gray-500 aspect-square"
+              className="object-contain w-full duration-200 cursor-pointer hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg shadow-md rounded hover: dark:bg-gray-500 aspect-square"
             />{" "}
             </div>
             ))
@@ -28,7 +29,8 @@ export default function FilasGaleria({ arrayImagenes }) {
       <div className="h-1/2  w-full mx-auto">
         <div className=" relative hover:saturate-100 sature-50 rounded  hover:border-red-500/80 bg-transparent duration-150 hover:border-dotted border-2 border-inherit">
           <Image
-          fill
+          width={500}
+          height={500}
             alt={"foto"}
             className="w-full h-64 object-cover rounded mx-auto duration-200 cursor-pointer hover:-translate-x-2 hover:-translate-y-2 hover:shadow-lg hover:saturate-100 sature-50  shadow-md "
             src={arrayImagenes? arrayImagenes[2]?.image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Pr%C3%A4torianer.jpg/250px-Pr%C3%A4torianer.jpg"}
--- src/app/components/Main/component/MultimediaGaleriaFotos.jsx
+++ src/app/components/Main/component/MultimediaGaleriaFotos.jsx
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 import { useQuery } from "@tanstack/react-query";
 import FilasGaleria from "./FilasGaleria";
-import { fetchGoogleGaleria, fetchGoogleNews } from "@/app/api/services/fetchGoogleSheet";
+import {
+  fetchGoogleGaleria,
+  fetchGoogleNews,
+} from "@/app/api/services/fetchGoogleSheet";
 import { useState } from "react";
 
 export default function MultimediaGaleriaFotos() {
-
-const [images, setImages] = useState(false)
-  const {data,error}=useQuery(['Galeria'],fetchGoogleGaleria)
+  const [images, setImages] = useState(false);
+  const { data, error } = useQuery(["Galeria"], fetchGoogleGaleria);
 
   return (
-    <div className="container  my-14 flex md:flex-row flex-col items-center group  justify-center gap-6 mx-auto">
-    
-    <FilasGaleria arrayImagenes={data?.data?.reverse().slice(0,3)}/>
-    <FilasGaleria arrayImagenes={data?.data?.reverse().slice(3,7)}/>
+    <div className="container h-full my-14 flex md:flex-row flex-col items-center group  justify-center gap-6 mx-auto">
+      <FilasGaleria arrayImagenes={data?.data?.reverse().slice(0, 3)} />
+      <FilasGaleria arrayImagenes={data?.data?.reverse().slice(3, 7)} />
     </div>
   );
 }
