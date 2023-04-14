130ba0a7f319f2fdc3b8dedba12641abf796368a
ramirochangomoreno <rama.exe.13@gmail.com>
2023-04-14 10:29:31 -0300

    corrigiendo extenciones

--- src/app/components/Skeletor.jsx
+++ src/app/components/Skeletor.jsx
@@ -5,10 +5,10 @@ export default function Skeletor() {
     <section className="text-gray-600 min-h-screen flex  bg-gray-50 w-full items-center justify-center z-40">
     <div className="flex flex-col m-8 rounded shadow-md w-full bg-gray-400 animate-pulse h-screen">
       <div className="h-full rounded-t bg-gray-700"></div>
-      <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 bg-gray-900">
-        <div className="w-full h-6 rounded bg-gray-700"></div>
-        <div className="w-full h-6 rounded bg-gray-700"></div>
-        <div className="w-3/4 h-6 rounded bg-gray-700"></div>
+      <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 animate-pulse bg-gray-900">
+        <div className="w-full h-6 rounded animate-pulse bg-gray-700"></div>
+        <div className="w-full h-6 rounded animate-pulse bg-gray-700"></div>
+        <div className="w-3/4 h-6 rounded animate-pulse bg-gray-700"></div>
       </div>
     </div>
     </section>
