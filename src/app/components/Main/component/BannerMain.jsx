7908ad245382bad20c6302de6a89861cd82577cb
ramirochangomoreno <rama.exe.13@gmail.com>
2023-07-13 14:37:40 -0300

    cambios de sponsor

--- src/app/components/Main/component/BannerMain.jsx
+++ src/app/components/Main/component/BannerMain.jsx
@@ -1,41 +1,24 @@
 import Image from "next/image";
-import chacon from "../../../../../public/Chacon.png";
-import chononos from "../../../../../public/Chononos.png";
-import Cifra from "../../../../../public/Cifra.png";
-import MasDeporte from "../../../../../public/MasDeporte.png";
-import shell from "../../../../../public/shell.png";
-import Fusion from "../../../../../public/Fusion.png";
+import Bichito from "../../../../../public/Bichito.jpg";
+import MuniFer from "../../../../../public/MuniFer.jpg";
+import MyM from "../../../../../public/MyM.jpg";
 
 export default function BannerMain() {
   const arraySponsor = [
+    
     {
-      src: chacon,
-      alt: "chacon",
-      id: 1,
+      src: MuniFer,
+      alt: "MuniFer",
+      id: 4,
     },
     {
-      src: chononos,
-      alt: "Chonono",
-      id: 2,
-    },
-    {
-      src: Cifra,
-      alt: "/cifra",
-      id: 3,
-    },
-    {
-      src: Fusion,
-      alt: "Fusion",
-      id: "4",
-    },
-    {
-      src: MasDeporte,
-      alt: "MasDeporte",
+      src: Bichito,
+      alt: "Bichito",
       id: 5,
     },
     {
-      src: shell,
-      alt: "shell",
+      src: MyM,
+      alt: "MyM",
       id: 6,
     },
   ];
@@ -45,12 +28,11 @@ export default function BannerMain() {
       <div className="relative w-full flex flex-wrap mx-auto items-center justify-around md:gap-4 gap-2  p-2">
         {arraySponsor.map((sponsor) => (
           <Image
-            height={100}
-            width={100}
+            
             quality={75}
             priority
             key={sponsor.id}
-            className="md:h-48 h-24 aspect-video  object-coveer object-center bg-gray-50 md:w-[25%] w-[45%] rounded-md p-2"
+            className="md:h-48 h-24 aspect-video  object-cover object-center bg-gray-50 md:w-[25%] w-[45%] rounded-md p-2"
             src={sponsor.src}
             alt={sponsor.alt}
           />
